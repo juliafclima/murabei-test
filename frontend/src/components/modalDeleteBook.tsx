a3d473131d9b78ccb54f6c930ca36634ac2d562e
Júlia Lima <juliafclima@hotmail.com>
2026-01-23 15:51:23 -0300

    fix(frontend): hydration error

--- frontend/src/components/modalDeleteBook.tsx
+++ frontend/src/components/modalDeleteBook.tsx
@@ -37,19 +37,17 @@ export default function ModalDeleteBook({ id, title }: BookDelete) {
    return (
       <div className="flex justify-end pr-4">
          <AlertDialog>
-            <AlertDialogTrigger>
-               <>
-                  <Tooltip>
-                     <TooltipTrigger asChild>
-                        <Button variant="ghost" size="icon">
-                           <Trash2 className="h-4 w-4 text-destructive" />
-                        </Button>
-                     </TooltipTrigger>
-                     <TooltipContent>
-                        <p>Delete book</p>
-                     </TooltipContent>
-                  </Tooltip>
-               </>
+            <AlertDialogTrigger asChild>
+               <Tooltip>
+                  <TooltipTrigger asChild>
+                     <Button variant="ghost" size="icon">
+                        <Trash2 className="h-4 w-4 text-destructive" />
+                     </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                     <p>Delete book</p>
+                  </TooltipContent>
+               </Tooltip>
             </AlertDialogTrigger>
 
             <AlertDialogContent>
