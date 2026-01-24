3239885e4f562600a8f04a9ba817e04afe0c44d8
Júlia Lima <juliafclima@hotmail.com>
2026-01-24 16:35:52 -0300

    feat(frontend): favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,20 @@
+import { Metadata } from "next";
+
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
-
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/sonner";
-
 import { BookProvider } from "./context/bookContext";
+
+export const metadata: Metadata = {
+  title: 'MyBook',
+  description: 'Book manager',
+  icons: {
+    icon: '/favicon.png'
+  },
+}
 
 export default function RootLayout({
   children,
