3f39f7d70f418b698e32126c7ec793e5c0bb13f2
Júlia Lima <juliafclima@hotmail.com>
2026-01-24 09:33:36 -0300

    feat(frontend): header and footer fixed

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
    return (
-      <footer className="border-t">
+      <footer className="fixed bottom-0 left-0 w-full border-t bg-background z-50">
          <div className="container mx-auto px-4 py-4 text-center text-sm text-muted-foreground">
             © MyBook. All rights reserved. Developed by{" "}
             <Link
--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -1,17 +1,35 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { BookOpenText } from "lucide-react";
 
 import SearchBy from "./searchBy";
+import { cn } from "@/lib/utils";
 
 export default function Header() {
    const pathname = usePathname();
    const isNewBookPage = pathname === "/new-book";
 
+   const [isScrolled, setisScrolled] = useState(false);
+
+   useEffect(() => {
+      const onScroll = () => {
+         setisScrolled(window.scrollY > 0);
+      }
+
+      window.addEventListener("scroll", onScroll);
+      return () => window.removeEventListener("scroll", onScroll);
+   })
+
    return (
-      <header className="border-b">
+      <header className={cn(
+         "w-full z-50 transition-all duration-200 bg-amber-50",
+         isScrolled
+            ? "fixed top-0 left-0 border-b shadow-sm"
+            : "relative"
+      )} >
          <div className="container mx-auto flex flex-col gap-4 px-4 py-4 md:h-16 md:flex-row md:items-center md:justify-between md:gap-0">
             <Link href="/" className="flex  items-center gap-2 text-xl font-bold tracking-tight">
                <BookOpenText className="h-8 w-8" />  MyBook
